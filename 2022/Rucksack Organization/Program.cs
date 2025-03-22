6fbf089578311a554807b6bcd8fbf4360d16c48f
Soapy <soapytarantula@gmail.com>
2025-03-22 09:47:48 -0500

    Reupload and reworking of my Advent of Code challenges.

---
+++ 2022/Rucksack Organization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main()
+    {
+        //char[] lowercase = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        List<string> _firstHalf = new List<string>();
+        List<string> _secondHalf = new List<string>();
+        List<char> _firstLetters = new List<char>();
+        List<char> _secondLetters = new List<char>();
+        List<string> test = new List<string>();
+        string[] input = File.ReadAllLines("..\\..\\..\\bags.txt");
+
+        int fart = 0;
+
+        foreach (var bag in input)
+        {
+            _firstHalf.Add(bag.Substring(0, bag.Length / 2));
+            _secondHalf.Add(bag.Substring(bag.Length / 2));
+        }
+    }
+}
---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Day1;
+
+internal class Program
+{
+    private static string[] _input = File.ReadAllLines("input.txt");
+    private static List<int> _leftNumbers = new();
+    private static List<int> _rightNumbers = new();
+    static int TotalDistance = 0;
+    static int Similarity = 0;
+
+    private static void Main()
+    {
+        foreach (var line in _input)
+        {
+            _leftNumbers.Add(int.Parse(line[..5])); // get the first 5 characters from each line
+            _rightNumbers.Add(int.Parse(line[8..])); // get the last characters from each line after the white space
+        }
+        
+        // part 1, compare the numbers and find the difference between them all.
+        for (int i = 0; i < _leftNumbers.Count; i++)
+        {
+            int _difference = _leftNumbers[i] - _rightNumbers[i];
+            _difference = Math.Abs(_difference);
+            TotalDistance += _difference;
+        }
+        // part 2, find how many times each number appears in both lists, multiplying the first by that count.
+        foreach (var set in _leftNumbers)
+        {
+            if (_rightNumbers.Contains(set))
+            {
+                Similarity += set * _rightNumbers.FindAll(c => c == set).Count;
+            }
+        }
+        Console.WriteLine($"Similarity is: {Similarity} | Total difference is: {TotalDistance}");
+    }
+}
